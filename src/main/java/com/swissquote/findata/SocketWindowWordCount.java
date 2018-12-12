1288590e18e3b206cc6c84c83cd5e6cd458b15b3
obogoliy <oleksandr.bogoliy@swissquote.ch>
2018-12-12 09:21:22 +0100

    Added Redis Sink

--- src/main/java/com/swissquote/findata/SocketWindowWordCount.java
+++ src/main/java/com/swissquote/findata/SocketWindowWordCount.java
@@ -1,15 +1,19 @@
 package com.swissquote.findata;
+
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.ReduceFunction;
-import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.connectors.redis.RedisSink;
+import org.apache.flink.streaming.connectors.redis.common.config.FlinkJedisPoolConfig;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommand;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommandDescription;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisMapper;
 import org.apache.flink.util.Collector;
 
 /**
  * Implements a streaming windowed version of the "WordCount" program.
- *
  * <p>This program connects to a server socket and reads strings from the socket.
  * The easiest way to try this out is to open a text server (at port 12345)
  * using the <i>netcat</i> tool via
@@ -23,27 +27,11 @@ public class SocketWindowWordCount {
 
 	public static void main(String[] args) throws Exception {
 
-		// the host and the port to connect to
-		final String hostname;
-		final int port;
-		try {
-			final ParameterTool params = ParameterTool.fromArgs(args);
-			hostname = params.has("hostname") ? params.get("hostname") : "localhost";
-			port = params.getInt("port");
-		} catch (Exception e) {
-			System.err.println("No port specified. Please run 'SocketWindowWordCount " +
-					"--hostname <hostname> --port <port>', where hostname (localhost by default) " +
-					"and port is the address of the text server");
-			System.err.println("To start a simple text server, run 'netcat -l <port>' and " +
-					"type the input text into the command line");
-			return;
-		}
-
 		// get the execution environment
 		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
 		// get input data by connecting to the socket
-		DataStream<String> text = env.socketTextStream(hostname, port, "\n");
+		DataStream<String> text = env.socketTextStream("localhsot", 9000, "\n");
 
 		// parse the data, group it, window it, and aggregate the counts
 		DataStream<WordWithCount> windowCounts = text
@@ -67,8 +55,24 @@ public class SocketWindowWordCount {
 					}
 				});
 
-		// print the results with a single thread, rather than in parallel
-		windowCounts.print().setParallelism(1);
+		FlinkJedisPoolConfig conf = new FlinkJedisPoolConfig.Builder().setHost("127.0.0.1").build();
+		windowCounts
+				.addSink(new RedisSink<>(conf, new RedisMapper<WordWithCount>() {
+					@Override
+					public RedisCommandDescription getCommandDescription() {
+						return new RedisCommandDescription(RedisCommand.SET, "SET");
+					}
+
+					@Override
+					public String getKeyFromData(WordWithCount wordWithCount) {
+						return wordWithCount.getWord();
+					}
+
+					@Override
+					public String getValueFromData(WordWithCount wordWithCount) {
+						return String.valueOf(wordWithCount.getCount());
+					}
+				}));
 
 		env.execute("Socket Window WordCount");
 	}
@@ -83,7 +87,8 @@ public class SocketWindowWordCount {
 		public String word;
 		public long count;
 
-		public WordWithCount() {}
+		public WordWithCount() {
+		}
 
 		public WordWithCount(String word, long count) {
 			this.word = word;
@@ -93,6 +98,14 @@ public class SocketWindowWordCount {
 		@Override
 		public String toString() {
 			return word + " : " + count;
+		}
+
+		public String getWord() {
+			return word;
+		}
+
+		public long getCount() {
+			return count;
 		}
 	}
 }
